5bfd5653c1851e3f234e315b5ea0a31e7099f891
Justin Mahar <contact@justinmahar.com>
2024-12-11 21:26:50 -0500

    Add support for netlify redirects

---
+++ create-redirects.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const dotenv = require("dotenv");
+// Load environment variables from the .env file
+dotenv.config();
+
+const redirectsEnvVarName = "REDIRECTS";
+let redirects = undefined;
+try {
+  const val = JSON.parse(process.env[redirectsEnvVarName]);
+  if (Array.isArray(val)) {
+    redirects = val;
+  } else {
+    console.error("REDIRECTS must be a JSON array of strings.");
+  }
+} catch (e) {
+  console.error(e);
+}
+
+let redirectsContent = "/* /index.html 200";
+
+if (redirects) {
+  redirectsContent = redirects.join("\n");
+}
+
+const redirectsFilePath = "./build/_redirects";
+
+fs.writeFile(redirectsFilePath, redirectsContent, (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log("Redirects file written successfully to:", redirectsFilePath);
+});
